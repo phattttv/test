a8c9a2a39a5239bd94306e4edc6da9a7f6515ec3
phattttv <trinhphat1467@gmail.com>
2024-11-04 20:47:27 +0700

    ontaphehe

---
+++ src/ontap/Ontap.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ontap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author trinh
+ */
+public class Ontap {
+
+    List<tt> list;
+    public Ontap() {
+        this.list = new ArrayList<>();
+    }
+    public int add() {
+        return 0;
+    }
+
+    public int remove() {
+
+        return 0;
+    }
+
+    public int update() {
+
+        return 0;
+    }
+}
---
+++ src/ontap/tt.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ontap;
+
+/**
+ *
+ * @author trinh
+ */
+public class tt {
+    String name;
+    String pass;
+
+    public tt(String name, String pass) {
+        this.name = name;
+        this.pass = pass;
+    }
+
+    public tt() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    
+    
+}
